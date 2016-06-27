24d3ac8005e030cf225650794cb3cbc8ad977889
nono tize kod <djcase001@gmail.com>
2016-06-27 17:43:41 -0400

    Implements the dom object selection

--- module/dsm.js
+++ module/dsm.js
@@ -1,7 +1,7 @@
 var nono = (function(){
 
-	var tags = /\b(?:div|aside|section|article|span|a|ul|li|ol|header|footer|h1|h2|h3|h4|h5|h6|img|p|button|input|select|label|table|theader|tbody|td|tr)\b/;
-	var attributes = /\b(?:color|background-color|font-size|text-decoration)\b/;
+	var tags = /\b(?:div|aside|section|article|span|a|ul|li|ol|header|footer|h1|h2|h3|h4|h5|h6|img|p|button|input|select|label|table|theader|tbody|td|tr|body)\b/;
+	var attributes = /\b(?:color|background-color|font-size|text-decoration|border)\b/;
 	var recup = null;
 
 	function selection(arg){
@@ -9,18 +9,29 @@ var nono = (function(){
 			if(arg[0] === '.'){
 				arg = arg.substr(1);
 				recup = document.getElementsByClassName(arg);
-				return this;
+				if(recup.length === 1){
+					recup = recup[0];
+				}else if(recup.length > 1){
+					return recup;
+				}
 			}else if(arg[0] === '#'){
 				arg = arg.substr(1);
 				recup = document.getElementById(arg);
-				return this;
 			}else if(tags.test(arg)){
 				recup = document.getElementsByTagName(arg);
-				return this; 
+				if(recup.length === 1){
+					recup = recup[0];
+				}else if(recup.length > 1){
+					return recup;
+				}
 			}else{
 				return undefined;
 			}
+
+		}else if(typeof arg === 'object'){
+			recup = arg;
 		}
+		return this;
 	}
 
 	function style(attr, prop){
@@ -33,6 +44,9 @@ var nono = (function(){
 				return this;
 			}else if(attr === 'text-decoration'){
 				recup.style.textDecoration = prop;
+				return this;
+			}else if(attr === 'border'){
+				recup.style.border = prop;
 				return this;
 			}else if(attr === 'font-size'){
 				if(typeof prop === 'number'){
