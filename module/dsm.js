f7643f71e7620bc7c596b4ce9aaea42849f96892
nono tize kod <djcase001@gmail.com>
2016-07-08 03:38:59 -0400

    First : erases the existing functions, changes the main function name to $noon. Second : adds two new functions CSS and ATTR.

--- module/dsm.js
+++ module/dsm.js
@@ -1,124 +1,43 @@
-var nono = (function(){
-
-	var tags = /\b(?:div|aside|section|article|span|a|ul|li|ol|header|footer|h1|h2|h3|h4|h5|h6|img|p|button|input|select|label|table|thead|tbody|th|td|tr|body|caption)\b/;
-	var attributes = /\b(?:color|background-color|font-size|text-decoration|border|font-style|width)\b/;
-	var embeds = /\b(?:src|id|class|href)\b/;
-	var recup = null;
-
-	function selection(arg){
-		if(typeof arg === 'string'){
-			if(arg[0] === '.'){  // If the selection is by string of a specific class
-				arg = arg.substr(1);
-				recup = document.getElementsByClassName(arg);
-				if(recup.length === 1){ // if there's only one element selected
-					recup = recup[0];
-				}else if(recup.length > 1){ // if there's multiple elements selected
-					return recup;
-				}
-			}else if(arg[0] === '#'){ // If the selection is by string of an id
-				arg = arg.substr(1);
-				recup = document.getElementById(arg);
-			}else if(tags.test(arg)){  // If the selection is by string of an html tag
-				recup = document.getElementsByTagName(arg);
-				if(recup.length === 1){  // if there's only one element selected
-					recup = recup[0];
-				}else if(recup.length > 1){  // if there's multiple elements selected
-					return recup;
-				}
-			}else{
-				return undefined;
-			}
-
-		}else if(typeof arg === 'object'){ // If the selection is a DOM object
-			recup = arg;
-		}
-		return this;
+function noon(selector, all){
+	var nono = {};
+	nono.selected = selector;
+	if(!all){
+		nono.element = document.querySelector(nono.selected);
+	}else if(all === '.'){
+		nono.element = document.querySelectorAll(nono.selected);
 	}
 
-	function embed(attr, prop){
-		if(embeds.test(attr)){
-			if(attr === 'src'){
-				recup.src = prop;
-			}
-		}if(embeds.test(attr)){
-			if(attr === 'href'){
-				recup.href = prop;
-			}
-		}
-		return this;
-	}
-
-	function style(attr, prop){
-		if(attributes.test(attr)){
-			if(attr === 'color'){
-				recup.style.color = prop;
-				return this;
-			}else if(attr === 'background-color'){
-				recup.style.backgroundColor = prop;
-				return this;
-			}else if(attr === 'text-decoration'){
-				recup.style.textDecoration = prop;
-				return this;
-			}else if(attr === 'font-style'){
-				recup.style.fontStyle = prop;
-				return this;
-			}else if(attr === 'border'){
-				recup.style.border = prop;
-				return this;
-			}else if(attr === 'width'){
-				recup.style.width = prop;
-				return this;
-			}else if(attr === 'height'){
-				recup.style.height = prop;
-				return this;
-			}else if(attr === 'font-size'){
-				if(typeof prop === 'number'){
-					recup.style.fontSize = prop+'px';
-				}else if(typeof prop === 'string'){
-					recup.style.fontSize = prop;
-				}
-				return this;
-			}
-		}
-	}
-
-	function create(tag){
-		if(typeof tag === 'string'){
-			if(tags.test(tag)){
-				recup = document.createElement(tag);
-				return recup;
-			}else{
-				return undefined;
-			}
-		}
-	}
-
-	function display(obj, msg){
-		if(typeof obj === 'object'){
-			obj.innerText = msg;
-			obj.textContent = msg;
+	nono.attr = function(name, value){
+		if(!value){
+			return nono.element.getAttribute(name);
 		}else{
-			recup.innerText = arguments[0];
-			recup.textContent = arguments[0];
+			nono.element.setAttribute(name,value);
 		}
-		return this;
+		return nono;
 	}
 
-	function stick(){
-		if(arguments.length === 1){
-			document.body.appendChild(arguments[0]);
-		}else if(arguments.length === 2){
-			arguments[0].appendChild(arguments[1]);
+	nono.css = function(prop, value){
+		if(!value){
+			if(nono.element.length > 1){
+				var tab = [];
+				for(var i=0, len = nono.element.length; i<len; i++){
+					tab.push(getComputedStyle(nono.element[i], null)[prop]);
+				}
+				return tab;
+			}else{
+				return getComputedStyle(nono.element, null)[prop];
+			}
+		}else{
+			if(nono.element.length > 1){
+				for(var i=0, len = nono.element.length; i<len; i++){
+					nono.element[i].style[prop] = value;
+				}
+			}else{
+				nono.element.style[prop] = value;
+			}
 		}
-		return this;
+		return nono;
 	}
 
-	return{
-		select  : selection,
-		css     : style,
-		embed   : embed,
-		create  : create,
-		display : display,
-		stickTo : stick
-	};
-})();
+	return nono;
+}
