f43678bece28ff473c2cc6e210d38c4f4da6c390
nono tize kod <djcase001@gmail.com>
2017-05-29 16:52:53 -0400

    Creates $noon and GetorMakeDom constructor

--- module/$noon.js
+++ module/$noon.js
@@ -1,6 +1,14 @@
 (function(win){
-    
+
     let global  = win;
     let doc     = this.document;
+
+    let $noon   = function(params, context){
+        return new GetorMakeDom(params, context);
+    }; // creating the $noon object
+
+    let GetorMakeDom = function(params, context){
+
+    }; // Creating the GetorMakeDom constructor
 
 })(window); // Creating a unique scope
